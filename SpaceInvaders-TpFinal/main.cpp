5189007b17695478feb734357e783a3970cffd3b
Enzo Serafini <enzo.serafini.01@gmail.com>
2025-11-11 15:28:09 -0300

    Ajustes de nave y creacion de la clase bala

--- SpaceInvaders-TpFinal/main.cpp
+++ SpaceInvaders-TpFinal/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <conio2.h>
+#include <ctime>
 using namespace std;
 
 const int LIM_IZQ = 5;
@@ -8,55 +9,101 @@ const int LIM_SUP = 2;
 const int LIM_INF = 28;
 
 void dibujarBordes() {
-	
 	for (int x = LIM_IZQ; x <= LIM_DER; x++) {
-		putchxy(x, LIM_SUP, '-');  
-		putchxy(x, LIM_INF, '-');  
+		putchxy(x, LIM_SUP, '-');
+		putchxy(x, LIM_INF, '-');
 	}
-	
 	for (int y = LIM_SUP; y <= LIM_INF; y++) {
-		putchxy(LIM_IZQ, y, '|');  
-		putchxy(LIM_DER, y, '|');  
+		putchxy(LIM_IZQ, y, '|');
+		putchxy(LIM_DER, y, '|');
 	}
 }
 
 class Nave {
-private:
 	int x;
 	int y;
+	int dir; 
+	clock_t tempo;
+	clock_t paso;
 public:
 	Nave() {
-		x = (LIM_IZQ + LIM_DER) / 2; 
-		y = LIM_INF - 1;            
+		x = (LIM_IZQ + LIM_DER) / 2;
+		y = LIM_INF - 1;
+		dir = 0;
+		paso = CLOCKS_PER_SEC / 30;  
+		tempo = clock();
 	}
+	
+	int getX() { return x; }
+	int getY() { return y; }
 	
 	void dibujar() {
 		textcolor(RED);
-		putchxy(x, y, '^'); 
+		gotoxy(x, y);
+		cout << "[^]";
 	}
-	
 	void borrar() {
-		putchxy(x, y, ' ');
+		gotoxy(x, y);
+		cout << "   ";
 	}
 	
+	void procesarTecla(int tecla) {
+		switch (tecla) {
+		case 75: dir = -1; break; 
+		case 77: dir = 1; break;  
+		}
+	}
+	
+	void actualizarMovimiento() {
+		if (tempo + paso < clock()) {  
+			if (dir == -1 && x > LIM_IZQ + 1) {
+				borrar();
+				x--;
+				dibujar();
+			} else if (dir == 1 && x < LIM_DER - 3) {
+				borrar();
+				x++;
+				dibujar();
+			}
+			tempo = clock();  
+		}
+	}
+};
+
+
+
+class Bala {
+	int x;
+	int y;
+	bool activa;
+	clock_t tempo;
+	clock_t paso;
+public:
+	Bala() {
+		activa = false;
+		paso = CLOCKS_PER_SEC / 40;  
+		tempo = clock();
+	}
+	void disparar(int _x, int _y) {
+		if (!activa) {
+			x = _x;
+			y = _y - 1;
+			activa = true;
+			putchxy(x, y, '|');
+			tempo = clock();
+		}
+	}
 	void mover() {
-		if (kbhit()) {
-			int tecla = getch();
-			switch (tecla) {
-			case 75:
-				if (x > LIM_IZQ + 1) {
-					borrar();
-					x--;
-					dibujar();
+		if (activa) {
+			if (tempo + paso < clock()) {
+				putchxy(x, y, ' ');
+				y--;
+				if (y <= LIM_SUP + 1) {
+					activa = false;
+				} else {
+					putchxy(x, y, '|');
 				}
-				break;
-			case 77: 
-				if (x < LIM_DER - 1) {
-					borrar();
-					x++;
-					dibujar();
-				}
-				break;
+				tempo = clock();
 			}
 		}
 	}
@@ -65,10 +112,27 @@ public:
 int main() {
 	dibujarBordes();
 	Nave n1;
+	Bala b1;
 	n1.dibujar();
 	
 	while (true) {
-		n1.mover();
+		
+		
+		if (kbhit()) {
+			int t = getch();
+			if (t == ' ') {
+				b1.disparar(n1.getX() + 1, n1.getY());
+			} else {
+				n1.procesarTecla(t);   
+			}
+		}
+		
+	
+		n1.actualizarMovimiento();
+		
+		b1.mover();
 	}
+	
 	return 0;
 }
+
